17141a66a2ab5600d5412b7b6fc2f6d43f812805
Nhan <nhan4@msn.com>
2022-12-15 08:38:10 -0800

    ls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'fe-story-points-estimator';
+  constructor(private http: HttpClient) {}
+  public ngOnInit(): void {
+    this.http
+      .get('http://localhost:3000/')
+      .subscribe((data) => console.log(data));
+  }
 }
